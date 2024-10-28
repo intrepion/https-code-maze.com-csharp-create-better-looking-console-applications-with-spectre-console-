56f2164cfe0b4673bf88fc1b066324dc6c2d510f
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 19:50:31 -0600

    dotnet format

--- HostelStudents.Shared/ExampleJsInterop.cs
+++ HostelStudents.Shared/ExampleJsInterop.cs
@@ -1,4 +1,4 @@
-using Microsoft.JSInterop;
+﻿using Microsoft.JSInterop;
 
 namespace HostelStudents.Shared;
 
@@ -15,7 +15,7 @@ public class ExampleJsInterop : IAsyncDisposable
 
     public ExampleJsInterop(IJSRuntime jsRuntime)
     {
-        moduleTask = new (() => jsRuntime.InvokeAsync<IJSObjectReference>(
+        moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
             "import", "./_content/HostelStudents.Shared/exampleJsInterop.js").AsTask());
     }
 
