b326f832eb640c01c5f0d299abec836b7d31cd65
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 20:01:34 -0600

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"HostelStudents\") }; { print }" HostelStudents.BusinessLogic/Repositories/Admin/*

--- HostelStudents.BusinessLogic/Repositories/Admin/IStudentAdminRepository.cs
+++ HostelStudents.BusinessLogic/Repositories/Admin/IStudentAdminRepository.cs
@@ -1,6 +1,6 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Entities.Dtos.Admin;
+﻿using HostelStudents.BusinessLogic.Entities.Dtos.Admin;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Repositories.Admin;
+namespace HostelStudents.BusinessLogic.Repositories.Admin;
 
 public interface IStudentAdminRepository
 {
