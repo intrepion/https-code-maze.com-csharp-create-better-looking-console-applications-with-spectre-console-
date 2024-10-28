872ff9ca328338528a6ebb47c5679a9f99b8d86d
Oliver Forral <oliver.forral@insightglobal.com>
2024-10-27 20:00:13 -0600

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"public string NormalizedFirstName { get; set; } = string.Empty;\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Student.cs

--- HostelStudents.BusinessLogic/Entities/Student.cs
+++ HostelStudents.BusinessLogic/Entities/Student.cs
@@ -8,5 +8,6 @@ public class Student
     public int Age { get; set; }
     public ApplicationUser? ApplicationUser { get; set; }
     public string FirstName { get; set; } = string.Empty;
+    public string NormalizedFirstName { get; set; } = string.Empty;
     // ActualPropertyPlaceholder
 }
