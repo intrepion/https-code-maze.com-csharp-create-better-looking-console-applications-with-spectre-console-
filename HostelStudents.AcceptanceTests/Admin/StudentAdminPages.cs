74185fddcc4157db869623bcc7ab04f2ab402625
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-04 13:59:30 -0700

    gawk -i inplace "{ gsub(/\/\/ CreatePropertyCodePlaceholder/, \"await Page.GetByLabel(\\\"Age:\\\").FillAsync(\\\"1\\\");\n\ \ \ \ \ \ \ \ // CreatePropertyCodePlaceholder\") }; { print }" HostelStudents.AcceptanceTests/Admin/StudentAdminPages.cs

--- HostelStudents.AcceptanceTests/Admin/StudentAdminPages.cs
+++ HostelStudents.AcceptanceTests/Admin/StudentAdminPages.cs
@@ -19,6 +19,7 @@ public partial class StudentAdminPages : PageTest
         await Page.GetByRole(AriaRole.Link, new() { Name = "Create New" }).ClickAsync();
         await Expect(Page).ToHaveTitleAsync("Student Add");
 
+        await Page.GetByLabel("Age:").FillAsync("1");
         // CreatePropertyCodePlaceholder
 
         await Page.GetByRole(AriaRole.Button, new() { Name = "Submit" }).ClickAsync();
