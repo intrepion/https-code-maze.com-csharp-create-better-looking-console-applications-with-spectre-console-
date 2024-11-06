912e14fb767ba419a0ef83788387c9de4b77d999
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:19:34 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"HostelStudents\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs

--- HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs
@@ -1,9 +1,9 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
-using ApplicationNamePlaceholder.BusinessLogic.Entities;
+using HostelStudents.BusinessLogic.Entities;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace HostelStudents.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
 
 // Creates the correct expressions to filter and sort.
 public class EntityNamePlaceholderGridQueryAdapter
