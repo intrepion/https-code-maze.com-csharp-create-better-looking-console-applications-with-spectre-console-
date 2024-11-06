4d6c57dbb368f621b152834489c2c2f6f3ee118f
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:41:07 -0700

    Styling Console Output

--- HostelStudents.ConsoleProject/Program.cs
+++ HostelStudents.ConsoleProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HostelStudents.BusinessLogic.Entities;
+using Spectre.Console;
 
 Console.WriteLine("Hello, World!");
 
@@ -18,3 +19,13 @@ Student[] _students = [
     new Student { Id = Guid.NewGuid(), FirstName = "Sabrina", LastName = "Azulon", Age = 18, Hostel = _hostels[0] },
     new Student { Id = Guid.NewGuid(), FirstName = "Hunter", LastName = "Cyril", Age = 19, Hostel = _hostels[0] },
 ];
+
+AnsiConsole.Markup($"[bold blue]Hello[/] [italic green]{_students[1].FirstName}[/]!");
+AnsiConsole.Write(new Markup($"[underline #800080]{_students[2].FirstName}[/]"));
+AnsiConsole.MarkupLine($"[rgb(128,0,0)]{_students[3].FirstName}[/]");
+
+AnsiConsole.MarkupLine($"[red on yellow] Hello, {_students[4].LastName}![/]");
+
+AnsiConsole.Markup($"[[{_students[3].FirstName}]]");
+AnsiConsole.MarkupLine($"[blue][[{_students[3].Hostel}]][/]");
+AnsiConsole.MarkupLine($"[{_students[3].Age}]".EscapeMarkup());
