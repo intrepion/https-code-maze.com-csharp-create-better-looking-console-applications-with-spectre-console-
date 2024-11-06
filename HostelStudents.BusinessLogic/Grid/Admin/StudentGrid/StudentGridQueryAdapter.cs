e284994444ccfe61e59059561cda12262333e6fa
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:19:57 -0700

    gawk -i inplace "{ gsub(/\/\/ SortExpressionCodePlaceholder/, \"{ StudentFilterColumns.Age, x => x != null ? x.Age.ToString() : string.Empty },\n\ \ \ \ \ \ \ \ \ \ \ \ // SortExpressionCodePlaceholder\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs

--- HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/StudentGridQueryAdapter.cs
@@ -17,6 +17,7 @@ public class StudentGridQueryAdapter
         {
             { StudentFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
 
+            { StudentFilterColumns.Age, x => x != null ? x.Age.ToString() : string.Empty },
             // SortExpressionCodePlaceholder
         };
 
