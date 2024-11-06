5d1799045df4dac5ecd6f04c5a27724cdf619e5b
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:18:33 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Hostel\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/HostelGrid/HostelGridQueryAdapter.cs

--- HostelStudents.BusinessLogic/Grid/Admin/HostelGrid/HostelGridQueryAdapter.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/HostelGrid/HostelGridQueryAdapter.cs
@@ -3,29 +3,29 @@ using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using HostelStudents.BusinessLogic.Entities;
 
-namespace HostelStudents.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+namespace HostelStudents.BusinessLogic.Grid.Admin.HostelGrid;
 
 // Creates the correct expressions to filter and sort.
-public class EntityNamePlaceholderGridQueryAdapter
+public class HostelGridQueryAdapter
 {
     // Holds state of the grid.
-    private readonly IEntityNamePlaceholderFilters controls;
+    private readonly IHostelFilters controls;
 
     // Expressions for sorting.
-    private readonly Dictionary<EntityNamePlaceholderFilterColumns, Expression<Func<EntityNamePlaceholder, string>>> expressions =
+    private readonly Dictionary<HostelFilterColumns, Expression<Func<Hostel, string>>> expressions =
         new()
         {
-            { EntityNamePlaceholderFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
+            { HostelFilterColumns.Id, x => !x.Id.Equals(Guid.Empty) ? x.Id.ToString() : string.Empty },
 
             // SortExpressionCodePlaceholder
         };
 
     // Queryables for filtering.
-    private readonly Dictionary<EntityNamePlaceholderFilterColumns, Func<IQueryable<EntityNamePlaceholder>, IQueryable<EntityNamePlaceholder>>> filterQueries = [];
+    private readonly Dictionary<HostelFilterColumns, Func<IQueryable<Hostel>, IQueryable<Hostel>>> filterQueries = [];
 
     // Creates a new instance of the GridQueryAdapter class.
-    // controls: The IEntityNamePlaceholderFilters" to use.
-    public EntityNamePlaceholderGridQueryAdapter(IEntityNamePlaceholderFilters controls)
+    // controls: The IHostelFilters" to use.
+    public HostelGridQueryAdapter(IHostelFilters controls)
     {
         this.controls = controls;
 
@@ -33,16 +33,16 @@ public class EntityNamePlaceholderGridQueryAdapter
         filterQueries =
             new()
             {
-                { EntityNamePlaceholderFilterColumns.Id, x => x.Where(y => y != null && !y.Id.Equals(Guid.Empty) && this.controls.FilterText != null && y.Id.ToString().Contains(this.controls.FilterText) ) },
+                { HostelFilterColumns.Id, x => x.Where(y => y != null && !y.Id.Equals(Guid.Empty) && this.controls.FilterText != null && y.Id.ToString().Contains(this.controls.FilterText) ) },
 
                 // QueryExpressionCodePlaceholder
             };
     }
 
     // Uses the query to return a count and a page.
-    // query: The IQueryable{EntityNamePlaceholder} to work from.
-    // Returns the resulting ICollection{EntityNamePlaceholder}.
-    public async Task<ICollection<EntityNamePlaceholder>> FetchAsync(IQueryable<EntityNamePlaceholder> query)
+    // query: The IQueryable{Hostel} to work from.
+    // Returns the resulting ICollection{Hostel}.
+    public async Task<ICollection<Hostel>> FetchAsync(IQueryable<Hostel> query)
     {
         query = FilterAndQuery(query);
         await CountAsync(query);
@@ -52,23 +52,23 @@ public class EntityNamePlaceholderGridQueryAdapter
     }
 
     // Get total filtered items count.
-    // query: The IQueryable{EntityNamePlaceholder} to use.
-    public async Task CountAsync(IQueryable<EntityNamePlaceholder> query) =>
+    // query: The IQueryable{Hostel} to use.
+    public async Task CountAsync(IQueryable<Hostel> query) =>
         controls.PageHelper.TotalItemCount = await query.CountAsync();
 
     // Build the query to bring back a single page.
-    // query: The <see IQueryable{EntityNamePlaceholder} to modify.
-    // Returns the new IQueryable{EntityNamePlaceholder} for a page.
-    public IQueryable<EntityNamePlaceholder> FetchPageQuery(IQueryable<EntityNamePlaceholder> query) =>
+    // query: The <see IQueryable{Hostel} to modify.
+    // Returns the new IQueryable{Hostel} for a page.
+    public IQueryable<Hostel> FetchPageQuery(IQueryable<Hostel> query) =>
         query
             .Skip(controls.PageHelper.Skip)
             .Take(controls.PageHelper.PageSize)
             .AsNoTracking();
 
     // Builds the query.
-    // root: The IQueryable{EntityNamePlaceholder} to start with.
-    // Returns the resulting IQueryable{EntityNamePlaceholder} with sorts and filters applied.
-    private IQueryable<EntityNamePlaceholder> FilterAndQuery(IQueryable<EntityNamePlaceholder> root)
+    // root: The IQueryable{Hostel} to start with.
+    // Returns the resulting IQueryable{Hostel} with sorts and filters applied.
+    private IQueryable<Hostel> FilterAndQuery(IQueryable<Hostel> root)
     {
         var sb = new System.Text.StringBuilder();
 
