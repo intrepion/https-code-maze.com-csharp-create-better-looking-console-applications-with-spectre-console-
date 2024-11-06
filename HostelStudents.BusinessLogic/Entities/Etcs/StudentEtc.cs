67c75c072a9fd5e63f1876ef72aab5703a828bf4
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:19:46 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Student\") }; { print }" HostelStudents.BusinessLogic/Entities/Etcs/StudentEtc.cs

--- HostelStudents.BusinessLogic/Entities/Etcs/StudentEtc.cs
+++ HostelStudents.BusinessLogic/Entities/Etcs/StudentEtc.cs
@@ -3,9 +3,9 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
 
-public class EntityNamePlaceholderEtc : IEntityTypeConfiguration<EntityNamePlaceholder>
+public class StudentEtc : IEntityTypeConfiguration<Student>
 {
-    public void Configure(EntityTypeBuilder<EntityNamePlaceholder> builder)
+    public void Configure(EntityTypeBuilder<Student> builder)
     {
         builder.ToTable("TableNamePlaceholder", x => x.IsTemporal());
 
