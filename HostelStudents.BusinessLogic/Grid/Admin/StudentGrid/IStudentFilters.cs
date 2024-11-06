231acc272b5429385d673d42289b0682790b5747
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-06 14:19:37 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Student\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/IStudentFilters.cs

--- HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/IStudentFilters.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/StudentGrid/IStudentFilters.cs
@@ -1,10 +1,10 @@
-﻿namespace HostelStudents.BusinessLogic.Grid.Admin.EntityNamePlaceholderGrid;
+﻿namespace HostelStudents.BusinessLogic.Grid.Admin.StudentGrid;
 
 // Interface for filtering.
-public interface IEntityNamePlaceholderFilters
+public interface IStudentFilters
 {
-    // The EntityNamePlaceholderFilterColumns being filtered on.
-    EntityNamePlaceholderFilterColumns FilterColumn { get; set; }
+    // The StudentFilterColumns being filtered on.
+    StudentFilterColumns FilterColumn { get; set; }
 
     // Loading indicator.
     bool Loading { get; set; }
@@ -18,6 +18,6 @@ public interface IEntityNamePlaceholderFilters
     // Gets or sets a value indicating if the sort is ascending or descending.
     bool SortAscending { get; set; }
 
-    // The EntityNamePlaceholderFilterColumns being sorted.
-    EntityNamePlaceholderFilterColumns SortColumn { get; set; }
+    // The StudentFilterColumns being sorted.
+    StudentFilterColumns SortColumn { get; set; }
 }
