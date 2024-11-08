0e9ff6b354e9a6326b47b535a89ecedff0b8f161
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-08 14:07:14 -0700

    dotnet new nunit --name HostelStudents.BusinessLogic.UnitTests

---
+++ HostelStudents.BusinessLogic.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace HostelStudents.BusinessLogic.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
