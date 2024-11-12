7bf8651cd22d68a6b3ea8dc22f4a2b76d82d51d0
Oliver Forral <oliver.forral@insightglobal.com>
2024-11-12 12:53:37 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"HostelStudents\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/IApplicationUserFilters.cs
@@ -1,4 +1,4 @@
-﻿namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+﻿namespace HostelStudents.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 // Interface for filtering.
 public interface IApplicationUserFilters
