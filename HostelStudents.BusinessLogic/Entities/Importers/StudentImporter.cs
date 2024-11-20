2510bbd984276563d9d7096d9c4a16151dae5a05
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:43:16 -0700

    gawk -i inplace "{ gsub(/\/\/ ExistingEntityCodePlaceholder/, \"dbStudent.Hostel = hostel;\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // ExistingEntityCodePlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs

--- HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
+++ HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
@@ -88,6 +88,7 @@ public static class StudentImporter
 
                     dbStudent.Age = record.Age;
                     dbStudent.FirstName = record.FirstName;
+                    dbStudent.Hostel = hostel;
                     // ExistingEntityCodePlaceholder
                 }
             }
