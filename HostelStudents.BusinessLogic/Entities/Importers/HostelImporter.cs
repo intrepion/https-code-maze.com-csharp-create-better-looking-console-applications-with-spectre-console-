a46961cde35d866084c16073f5cf42f7d30ccf59
Oliver Forral <intrepion@gmail.com>
2024-11-20 15:41:20 -0700

    gawk -i inplace "{ gsub(/\/\/ NewEntityCodePlaceholder/, \"Name = record.Name,\n\ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ \ // NewEntityCodePlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Importers/HostelImporter.cs

--- HostelStudents.BusinessLogic/Entities/Importers/HostelImporter.cs
+++ HostelStudents.BusinessLogic/Entities/Importers/HostelImporter.cs
@@ -58,6 +58,7 @@ public static class HostelImporter
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
+                    Name = record.Name,
                     // NewEntityCodePlaceholder
                 };
 
