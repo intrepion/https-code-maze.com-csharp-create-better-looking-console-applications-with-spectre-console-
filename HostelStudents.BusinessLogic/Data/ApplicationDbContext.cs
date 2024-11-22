15f8a32175b146a4a949c0fb8ea06131e1eb0abd
Oliver Forral <intrepion@gmail.com>
2024-11-22 11:05:00 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"HostelStudents\") }; { print }" HostelStudents.BusinessLogic/Data/*

--- HostelStudents.BusinessLogic/Data/ApplicationDbContext.cs
+++ HostelStudents.BusinessLogic/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using ApplicationNamePlaceholder.BusinessLogic.Entities;
-using ApplicationNamePlaceholder.BusinessLogic.Entities.Configuration;
+﻿using HostelStudents.BusinessLogic.Entities;
+using HostelStudents.BusinessLogic.Entities.Configuration;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ApplicationNamePlaceholder.BusinessLogic.Data;
+namespace HostelStudents.BusinessLogic.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser, ApplicationRole, Guid, ApplicationUserClaim, ApplicationUserRole, ApplicationUserLogin, ApplicationRoleClaim, ApplicationUserToken>(options)
 {
