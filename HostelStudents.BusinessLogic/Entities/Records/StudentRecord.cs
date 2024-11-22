1c7f54a632e28cebf6495da482d6a2f7fd1e3974
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:29:48 -0700

    gawk -i inplace "{ gsub(/\/\/ RecordPropertyCodePlaceholder/, \"public string Hostel_NormalizedName { get; set; } = string.Empty;\n\ \ \ \ // RecordPropertyCodePlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Records/StudentRecord.cs

--- HostelStudents.BusinessLogic/Entities/Records/StudentRecord.cs
+++ HostelStudents.BusinessLogic/Entities/Records/StudentRecord.cs
@@ -4,5 +4,6 @@ public class StudentRecord
 {
     public int Age { get; set; }
     public string FirstName { get; set; } = string.Empty;
+    public string Hostel_NormalizedName { get; set; } = string.Empty;
     // RecordPropertyCodePlaceholder
 }
