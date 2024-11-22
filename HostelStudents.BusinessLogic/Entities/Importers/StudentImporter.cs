e237ef605341b992f6f8dd34fff527b902e870e1
Oliver Forral <intrepion@gmail.com>
2024-11-21 20:28:05 -0700

    gawk -i inplace "{ gsub(/EntityNamePlaceholder/, \"Student\") }; { print }" HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs

--- HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
+++ HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ApplicationNamePlaceholder.BusinessLogic.Entities.Importers;
 
-public static class EntityNamePlaceholderImporter
+public static class StudentImporter
 {
     public static async Task ImportAsync(
        ApplicationDbContext context,
@@ -42,7 +42,7 @@ public static class EntityNamePlaceholderImporter
             Delimiter = "|",
         });
 
-        var records = csv.GetRecords<EntityNamePlaceholderRecord>();
+        var records = csv.GetRecords<StudentRecord>();
 
         // EntityListCodePlaceholder
 
@@ -54,25 +54,25 @@ public static class EntityNamePlaceholderImporter
                 // NullCheckCodePlaceholder
             )
             {
-                var LowercaseNamePlaceholder = new EntityNamePlaceholder
+                var LowercaseNamePlaceholder = new Student
                 {
                     ApplicationUserUpdatedBy = applicationUserUpdatedBy,
 
                     // NewEntityCodePlaceholder
                 };
 
-                var dbEntityNamePlaceholder = await context.TableNamePlaceholder.SingleOrDefaultAsync(
+                var dbStudent = await context.TableNamePlaceholder.SingleOrDefaultAsync(
                     x => true
                     // CompositeKeyCodePlaceholder
                 );
 
-                if (dbEntityNamePlaceholder is null)
+                if (dbStudent is null)
                 {
                     await context.TableNamePlaceholder.AddAsync(LowercaseNamePlaceholder);
                 }
                 else
                 {
-                    dbEntityNamePlaceholder.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
+                    dbStudent.ApplicationUserUpdatedBy = applicationUserUpdatedBy;
 
                     // ExistingEntityCodePlaceholder
                 }
