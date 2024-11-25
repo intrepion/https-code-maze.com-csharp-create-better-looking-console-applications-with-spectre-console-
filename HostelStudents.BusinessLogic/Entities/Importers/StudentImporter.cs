9dfafdaacbf58483b89ea8e6865a9406303c7b4c
Oliver Forral <intrepion@gmail.com>
2024-11-25 00:47:48 -0700

    gawk -i inplace "{ gsub(/\/\/ ManyToOneCodePlaceholder/, \"\ \ \ \ \\\\&\\\\& x.NormalizedName.Equals(record.Hostel_NormalizedName)\n\ \ \ \ \ \ \ \ \ \ \ \ // ManyToOneCodePlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs

--- HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
+++ HostelStudents.BusinessLogic/Entities/Importers/StudentImporter.cs
@@ -51,6 +51,7 @@ public static class StudentImporter
         {
             var hostel = hostelList.FirstOrDefault(x =>
                 true
+                && x.NormalizedName.Equals(record.Hostel_NormalizedName)
             // ManyToOneCodePlaceholder
 
             if (true
