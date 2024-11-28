383bf1aec09ead82df7ebb097e27f1a55037996b
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:08:30 -0700

    gawk -i inplace "{ gsub(/ApplicationNamePlaceholder/, \"HostelStudents\") }; { print }" HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/*

--- HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
+++ HostelStudents.BusinessLogic/Grid/Admin/ApplicationUserGrid/ApplicationUserGridControls.cs
@@ -1,4 +1,4 @@
-namespace ApplicationNamePlaceholder.BusinessLogic.Grid.Admin.ApplicationUserGrid;
+namespace HostelStudents.BusinessLogic.Grid.Admin.ApplicationUserGrid;
 
 public class ApplicationUserGridControls(IPageHelper pageHelper) : IApplicationUserFilters
 {
