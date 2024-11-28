bd545ad563c329b3495ef8c8d18ce40ef7da3e0b
Oliver Forral <intrepion@gmail.com>
2024-11-28 00:07:53 -0700

    gawk -i inplace "{ gsub(/\/\/ ActualPropertyPlaceholder/, \"[Required]\n\ \ \ \ // ActualPropertyPlaceholder\") }; { print }" HostelStudents.BusinessLogic/Entities/Student.cs

--- HostelStudents.BusinessLogic/Entities/Student.cs
+++ HostelStudents.BusinessLogic/Entities/Student.cs
@@ -14,5 +14,6 @@ public class Student
     public ApplicationUser? ApplicationUser { get; set; }
     [Required]
     public string FirstName { get; set; } = string.Empty;
+    [Required]
     // ActualPropertyPlaceholder
 }
