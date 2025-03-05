e8bdc6d901e119c4d5ba6008e3a4f3eb4bc5cc55
Oliver Forral <intrepion@gmail.com>
2025-03-05 03:32:05 -0700

    dotnet format

---
+++ HostelStudents/Data/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HostelStudents.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
