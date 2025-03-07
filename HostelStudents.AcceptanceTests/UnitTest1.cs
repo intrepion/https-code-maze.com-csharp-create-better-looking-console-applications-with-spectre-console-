f0018ec70ef9f683532df6b7a0b796d69df8f8bb
Oliver Forral <intrepion@gmail.com>
2025-03-07 00:14:48 -0700

    dotnet format

---
+++ HostelStudents.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace HostelStudents.AcceptanceTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
