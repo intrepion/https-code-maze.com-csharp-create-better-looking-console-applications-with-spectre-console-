85e0eb70567a077e49e6a52f9a44ea97a11f8d15
Oliver Forral <intrepion@gmail.com>
2025-03-07 00:16:20 -0700

    dotnet ef --project HostelStudents.BusinessLogic --startup-project HostelStudents migrations add Initial

---
+++ HostelStudents.BusinessLogic/Entities/Records/ApplicationUserRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace HostelStudents.BusinessLogic.Entities.Records;
+
+public class ApplicationUserRecord
+{
+    public string Email { get; set; } = string.Empty;
+    public bool EmailConfirmed { get; set; }
+    public string Password { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+
+    public string Student_NormalizedFirstName { get; set; } = string.Empty;
+    public string Student_NormalizedLastName { get; set; } = string.Empty;
+    // RecordPropertyCodePlaceholder
+}
